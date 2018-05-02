77877b4ff38bfbde871d1c895357c9bb6d870b62
ZAFORO-11 <ZAFORO-11@DESKTOP-0PUR0TK>
2018-05-02 17:08:25 +0200

    Este es el punto de inicio del proyecto.
    Es un ejemplo
    
    Change-Id: I496bb694dcc6ccc578c60e3870b77966a03e4b67
    Signed-off-by: ZAFORO-11 <ZAFORO-11@DESKTOP-0PUR0TK>

---
+++ EjemploGit/src/prueba/Clase1.java
@@ -0,0 +1,14 @@
+package prueba;
+
+public class Clase1 {
+
+	public Clase1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
